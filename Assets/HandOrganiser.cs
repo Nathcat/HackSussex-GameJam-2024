361416f4e295ddf5e6f5fb6425d774864d5bc2d7
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2024-11-30 17:48:33 +0000

    now i store in funky store

--- Assets/HandOrganiser.cs
+++ Assets/HandOrganiser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -12,13 +13,14 @@ public class HandOrganiser : MonoBehaviour
     [SerializeField] private float max_count;
     [SerializeField] private float start_pos;
     [SerializeField] private List<Card> cards = new List<Card>();
+    [SerializeField] private List<GameObject> funky_store = new List<GameObject>();
     private void Start()
     {
-        Generate(cards);
     }
 
     public void Generate(List<Card> cards) 
     {
+        funky_store = new List<GameObject>();
         width = card_prefab.GetComponent<Renderer>().bounds.size.x;
         
         float screenWidth = Camera.main.ScreenToWorldPoint(Camera.main.ViewportToScreenPoint(new Vector3(1f, 0f, 1f))).x - Camera.main.ScreenToWorldPoint(Camera.main.ViewportToScreenPoint(new Vector3(0f, 0f, 1f))).x;
@@ -34,6 +36,7 @@ public class HandOrganiser : MonoBehaviour
                 GameObject card_x = Instantiate(card_prefab);
                 card_x.GetComponent<CardRenderer>().Render(cards[index]);
                 card_x.transform.position = new Vector3((start_pos + (width / 2)), -3.38f, 1f);
+                funky_store.Add(card_x);
                 start_pos = start_pos + width;
             }
         }
@@ -43,6 +46,7 @@ public class HandOrganiser : MonoBehaviour
             card_x.GetComponent<CardRenderer>().Render(cards[0]);
             cards.RemoveAt(0);
             card_x.transform.position = new Vector3(0.0f, -3.38f, 1f);
+            funky_store.Add(card_x);
 
             start_pos = 0 - (((size / 2) * width) + (width / 2));
             for (int index = 0; index < cards.Count/2; index++)
@@ -51,6 +55,7 @@ public class HandOrganiser : MonoBehaviour
                 card_x.GetComponent<CardRenderer>().Render(cards[index]);
                 card_x.transform.position = new Vector3((start_pos + (width / 2)), -3.38f, 1f);
                 start_pos = start_pos + width;
+                funky_store.Add(card_x);
             }
 
             start_pos = 0 + (width/2);
@@ -60,15 +65,49 @@ public class HandOrganiser : MonoBehaviour
                 card_x.GetComponent<CardRenderer>().Render(cards[index]);
                 card_x.transform.position = new Vector3((start_pos + (width / 2)), -3.38f, 1f);
                 start_pos = start_pos + width;
+                funky_store.Add(card_x);
             }
-        }
-
-        for (int index = 0; index < cards.Count; index++) 
-        { 
-            
         }
     }
 
+    public void sort(List<GameObject> cards)
+    {
+        int size = cards.Count;
+        print(max_count);
+
+        if ((size % 2) == 0)
+        {
+            start_pos = 0 - ((size / 2) * width);
+            for (int index = 0; index < cards.Count; index++)
+            {
+                cards[index].transform.position = new Vector3((start_pos + (width / 2)), -3.38f, 1f);
+                start_pos = start_pos + width;
+                funky_store.Add(cards[index]);
+            }
+        }
+        else if (!((size % 2) == 0))
+        {
+            cards[0].transform.position = new Vector3(0.0f, -3.38f, 1f);
+            funky_store.Add(cards[0]);
+            cards.RemoveAt(0);
+            start_pos = 0 - (((size / 2) * width) + (width / 2));
+            for (int index = 0; index < cards.Count / 2; index++)
+            {
+
+                cards[index].transform.position = new Vector3((start_pos + (width / 2)), -3.38f, 1f);
+                start_pos = start_pos + width;
+                funky_store.Add(cards[index]);
+            }
+
+            start_pos = 0 + (width / 2);
+            for (int index = ((cards.Count / 2)); index < cards.Count; index++)
+            {
+                cards[index].transform.position = new Vector3((start_pos + (width / 2)), -3.38f, 1f);
+                start_pos = start_pos + width;
+                funky_store.Add(cards[index]);
+            }
+        }
+    }
 
 
 }

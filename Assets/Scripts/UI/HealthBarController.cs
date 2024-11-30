8566013886cd59c1efbfe6468d881330befa6511
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-11-30 18:48:06 +0000

    health and shield

---
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Slider defenceSlider;
+    [SerializeField] private Combatant combatant;
+
+    private void Start()
+    {
+        healthSlider.maxValue = combatant.getHealth();
+        defenceSlider.maxValue = combatant.getHealth();
+    }
+
+    private void Update()
+    {
+        healthSlider.value = combatant.getHealth();
+        defenceSlider.value = combatant.getDefence();
+    }
+}

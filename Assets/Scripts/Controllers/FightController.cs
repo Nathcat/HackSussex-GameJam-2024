00be25a5cd4930642518e17da753dffb3f44cdd3
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-30 15:54:46 +0000

    Started implemented basic fight scene.
    Also added defence reset when a combatant's turn starts

--- Assets/Scripts/Controllers/FightController.cs
+++ Assets/Scripts/Controllers/FightController.cs
@@ -10,6 +10,30 @@ public class FightController : MonoBehaviour
     public Combatant[] combatants;
     public int currentCombatant = 0;
 
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+    public Vector3 playerSpawnPosition;
+    public Vector3[] enemySpawnPositions;
+
+    public void Start() {
+        int enemyCount = Random.Range(1, 3);
+        combatants = new Combatant[enemyCount + 1];
+
+        combatants[0] = Instantiate(
+            playerPrefab,
+            playerSpawnPosition,
+            new Quaternion() 
+        ).GetComponent<Combatant>();
+
+        for (int i = 1; i < enemyCount + 1; i++) {
+            combatants[i] = Instantiate(
+                enemyPrefab,
+                enemySpawnPositions[i - 1],
+                new Quaternion()
+            );
+        }
+    }
+
     public void playTurn() {
         combatants[currentCombatant].startTurn();
     }

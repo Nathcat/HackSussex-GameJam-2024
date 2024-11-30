c81a08cb6e6f5c557d01060f50922d4feff31402
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-11-30 15:43:54 +0000

    animator rename

--- Assets/Scripts/SpriteAnimator.cs
+++ Assets/Scripts/SpriteAnimator.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Animator : MonoBehaviour
+public class SpriteAnimator : MonoBehaviour
 {
     [SerializeField] private Sprite[] frames;
     [SerializeField] private float framePeriod;

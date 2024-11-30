0b3458e1baf7310b6be61c97917dee01641851a9
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-11-30 15:30:44 +0000

    animator

---
+++ Assets/Scripts/Animator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frames;
+    [SerializeField] private float framePeriod;
+
+    private SpriteRenderer spriteRenderer;
+    private float frameTime = 0;
+    private int frameIndex = 0;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = frames[0];
+    }
+
+    void Update()
+    {
+        frameTime += Time.deltaTime;
+        if (frameTime >= framePeriod)
+        {
+            frameIndex++;
+            frameTime = 0;
+            if (frameIndex >= frames.Length) frameIndex = 0;
+            spriteRenderer.sprite = frames[frameIndex];
+        }
+    }
+}

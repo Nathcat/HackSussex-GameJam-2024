545de4fa3befbf7f654acc5ab1e66b8c3d14e98b
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-30 14:21:50 +0000

    Removed chooseCard method, replaced with starTurn and endTurn functions

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class EnemyController : Combatant
 {
-    /// <summary>
-    /// Choose a card randomly from the deck
-    /// </summary>
-    /// <returns>A randomly chosen card from the deck</returns>
-    /// <exception cref="DeckIsEmptyException">Thrown when the deck is empty</exception>
-    override public void chooseCard() {
-        if (base.getDeck().Length == 0) {
-            throw new DeckIsEmptyException();
-        }
-        
+    override public void startTurn() {
         base.chosenCard = base.getDeck()[
-            Random.Range(0, base.getDeck().Length - 1)
+            Random.Range(0, base.getDeck().Length - 1);
         ];
+
+        // Do stuff here ...
     }
 }
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class PlayerController : Combatant
 {
-    override public void chooseCard() {
-        // This function does nothing!
-
-        // Will be handled by the user dragging and dropping a card
-        // onto another combatant
+    override public void startTurn() {
+        // Do UI stuff here ...
     }
 }

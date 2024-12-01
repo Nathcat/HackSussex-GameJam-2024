b8f1a890a17f70af6a04de50c8a623adb8316264
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-12-01 04:41:56 +0000

    Implemented choice UI for moving between nodes in graph

--- Assets/Scripts/Graph/CameraController.cs
+++ Assets/Scripts/Graph/CameraController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     public GraphRenderer graphRenderer;
     public GameObject targetNode;
+    public int fuel = 10;
+    public Canvas canvas;
 
     public void Start() {
         targetNode = graphRenderer.graph.rootNode.obj;
@@ -16,7 +21,56 @@ public class CameraController : MonoBehaviour
         RaycastHit hit;
         if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity)) {
             if (hit.collider.gameObject.CompareTag("Node")) {
-                targetNode = hit.collider.gameObject;
+                if (hit.collider.gameObject == targetNode) {
+                    SceneManager.LoadScene("FightScene");
+                }
+
+                foreach (Edge e in targetNode.GetComponent<NodeComponent>().node.edges) {
+                    if (e.B.obj == hit.collider.gameObject) {
+                        if (fuel >= e.weight) {
+                            canvas.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = "Moving to this ship will require <b><i>" + e.weight + "</i></b> fuel.\n\nContinue?";
+                            canvas.transform.GetChild(1).gameObject.SetActive(true);
+                            canvas.transform.GetChild(2).gameObject.SetActive(true);
+                            canvas.transform.GetChild(3).gameObject.SetActive(false);
+                            
+                            canvas.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate {
+                                targetNode = hit.collider.gameObject;
+                                fuel -= e.weight;
+                                canvas.gameObject.SetActive(false);
+                            });
+
+                            canvas.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate {
+                                canvas.gameObject.SetActive(false);
+                            });
+
+                            canvas.gameObject.SetActive(true);
+                        }
+                        else {
+                            canvas.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = "Moving to this ship will require <b><i>" + e.weight + "</i></b> fuel.\n\nYou don't have enough fuel!";
+                            canvas.transform.GetChild(1).gameObject.SetActive(false);
+                            canvas.transform.GetChild(2).gameObject.SetActive(false);
+                            canvas.transform.GetChild(3).gameObject.SetActive(true);
+
+                            canvas.transform.GetChild(3).GetComponent<Button>().onClick.AddListener(delegate {
+                                canvas.gameObject.SetActive(false);
+                            });
+
+                            canvas.gameObject.SetActive(true);
+                        }
+                    }
+                }
+
+                foreach (Edge e in targetNode.GetComponent<NodeComponent>().node.backwardsEdges) {
+                    if (e.B.obj == hit.collider.gameObject) {
+                        if (fuel >= e.weight) {
+                            targetNode = hit.collider.gameObject;
+                            fuel -= e.weight;
+                        }
+                        else {
+                            Debug.LogError("Not enough fuel! Do some UI thing here?");
+                        }
+                    }
+                }
             }
         }
 
@@ -24,9 +78,13 @@ public class CameraController : MonoBehaviour
     }
 
     public void UpdateCameraPosition() {
-        Vector3 targetPosition = new Vector3(targetNode.transform.position.x, targetNode.transform.position.y, -10f);
+        Vector3 targetPosition = new Vector3(targetNode.transform.position.x, targetNode.transform.position.y, -20f);
         Vector3 difference = targetPosition - transform.position;
         
         transform.Translate(difference * 0.1f);
+    }
+
+    public void LoadLevel() {
+        SceneManager.LoadScene("FightScene");
     }
 }

93ec8559d1e532c09edf494cc1d2e00a0c99cb72
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2024-12-01 09:51:44 +0000

    help menu

---
+++ Assets/helpMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class helpMenu : MonoBehaviour
+
+
+{
+    public GameObject help_menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        help_menu.SetActive(false);
+    }
+
+    public void help() {
+        help_menu.SetActive(true);
+    }
+
+    public void rn() {
+        help_menu.SetActive(false);
+    }
+    // Update is called once per frame
+
+}

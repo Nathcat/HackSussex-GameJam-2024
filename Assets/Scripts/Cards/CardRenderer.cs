801da9ed8a25506b437e310c12f1976d8f28a938
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-12-01 02:04:33 +0000

    card enlarge

--- Assets/Scripts/Cards/CardRenderer.cs
+++ Assets/Scripts/Cards/CardRenderer.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class CardRenderer : MonoBehaviour
 {
+    [SerializeField] private float enlargeAmount;
     [SerializeField] private Card card;
     [SerializeField] private References references;
+    
+    private bool enlarged = false;
+    private Vector3 position;
+    private Vector3 scale;
 
     private void Start()
     {
+        position = transform.position;
+        scale = transform.localScale;
         Render(card);
+    }
+
+    private void OnMouseEnter()
+    {
+        enlarged = true;
+    }
+
+    private void OnMouseExit()
+    {
+        enlarged = false;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, scale * (enlarged ? enlargeAmount : 1), Time.deltaTime * 5);
+        transform.position = Vector3.Lerp(transform.position, enlarged ? new Vector3(position.x, -1, position.z - 0.1f) : position, Time.deltaTime * 5);
     }
 
     public Card GetCard() { return card; }

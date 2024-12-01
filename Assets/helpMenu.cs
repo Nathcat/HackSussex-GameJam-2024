6488dc3a9b713d919ba7333e591e208cb373a084
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2024-12-01 10:16:45 +0000

    made nathans scene cooler

--- Assets/helpMenu.cs
+++ Assets/helpMenu.cs
@@ -10,7 +10,7 @@ public class helpMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        help_menu.SetActive(false);
+        
     }
 
     public void help() {
